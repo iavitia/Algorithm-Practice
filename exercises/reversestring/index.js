1e8c9570d9d492f9e09bca188e6a7d68aade3c0c
iavitia <iavitia@outlook.com>
2018-12-05 16:59:33 -0700

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let newStr = str
+    .split("")
+    .reverse()
+    .join("");
+  return newStr;
+}
 
 module.exports = reverse;
