9cfc5785b11a8ac76e8a9f982d74a00bb91c7664
iavitia <iavitia@outlook.com>
2018-12-10 21:25:53 -0700

    mac char solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let character of str) {
+    !charMap[character] ? (charMap[character] = 1) : charMap[character]++;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
