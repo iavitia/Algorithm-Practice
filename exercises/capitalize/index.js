323fd5e09a443e77def2fee567fbda41ddd0030a
iavitia <iavitia@outlook.com>
2018-12-19 01:53:04 -0700

    capitalize first letters

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,16 +7,6 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-// function capitalize(str) {
-//   let arr = str.split(" ");
-//   const copy = [];
-
-//   arr.forEach(function(item) {
-//     copy.push(item[0].toUpperCase() + item.slice(1).toLowerCase());
-//   });
-
-//   return copy.join(" ");
-// }
 function capitalize(str) {
   const words = [];
 
@@ -28,3 +18,14 @@ function capitalize(str) {
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   let arr = str.split(" ");
+//   const copy = [];
+
+//   arr.forEach(function(item) {
+//     copy.push(item[0].toUpperCase() + item.slice(1).toLowerCase());
+//   });
+
+//   return copy.join(" ");
+// }S
